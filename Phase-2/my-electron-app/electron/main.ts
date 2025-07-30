289ae042a60a5f692b1e77497f64573ab4462989
Deepankar Sah <deepankerrocking@gmail.com>
2025-07-30 13:28:35 +0530

    Phase 2 Completed

---
+++ Phase-2/my-electron-app/electron/main.ts
@@ -0,0 +1,36 @@
+import { app, BrowserWindow } from "electron";
+import * as path from "path";
+
+// Creates window function
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      preload: path.join(__dirname, "preload.js"), // optional
+    },
+  });
+
+  // during dev use Vite's dev server
+  if (process.env.VITE_DEV_SERVER_URL) {
+    win.loadURL(process.env.VITE_DEV_SERVER_URL);
+    win.webContents.openDevTools(); // optional
+  } else {
+    win.loadFile(path.join(__dirname, "../index.html"));
+  }
+}
+
+// Load Window when it ready
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
